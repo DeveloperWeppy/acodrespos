c70b591cbcae546ad14ab6b28362314ffb5637fe
WeppyDev <104014309+DeveloperWeppy@users.noreply.github.com>
2022-12-02 16:09:26 -0500

    correcciones
    quitar boton de asignar conuctor al rol de mesero

--- app/Order.php
+++ app/Order.php
@@ -299,17 +299,20 @@ class Order extends Model
             }else if(in_array($lastStatusAlias,["accepted_by_admin"])){
                 return ["buttons"=>['rejected_by_restaurant','accepted_by_restaurant'],'message'=>""];
             }else if(in_array($lastStatusAlias,["assigned_to_driver","accepted_by_restaurant","accepted_by_driver","rejected_by_driver"])  ){
-                if(in_array($lastStatusAlias,["assigned_to_driver"]) && $this->delivery_method=="1" ){
+                
+                if(in_array($lastStatusAlias,["assigned_to_driver"]) && $this->delivery_method=="1" && !auth()->user()->hasRole('staff') ){
                     //In this case we can re-assign or deliver it
                     return ["buttons"=>['rejected_by_restaurant','delivered','assigned_to_driver'],'message'=>""];
                 }
                 return ["buttons"=>['rejected_by_restaurant','prepared'],'message'=>""];
 
             }else if(in_array($lastStatusAlias,["prepared"])&&(config('app.allow_self_deliver')||$this->restorant->self_deliver.""=="1")/*&&$this->delivery_method.""=="2"*/){
-                if($this->delivery_method=="2" || $this->delivery_method=="3"){
-                    return ["buttons"=>['rejected_by_restaurant','delivered'],'message'=>""];
+              
+                if($this->delivery_method=="1" && !auth()->user()->hasRole('staff') ){
+                    return ["buttons"=>['rejected_by_restaurant','delivered','assigned_to_driver'],'message'=>""];
                 }
-                return ["buttons"=>['rejected_by_restaurant','delivered','assigned_to_driver'],'message'=>""];
+                return ["buttons"=>['rejected_by_restaurant','delivered'],'message'=>""];
+                
             }else if(in_array($lastStatusAlias,["prepared"])&&$this->delivery_method.""=="2"){
                 return ["buttons"=>['delivered'],'message'=>""];
             }
