cfb219f2afac49a0fefbc94909921c0f3ae505e6
WeppyDev <104014309+DeveloperWeppy@users.noreply.github.com>
2022-12-13 12:23:01 -0500

    notificacion reservas

--- app/Models/Reservation.php
+++ app/Models/Reservation.php
@@ -4,10 +4,13 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Notifications\Notifiable;
 
 class Reservation extends Model
 {
     use HasFactory;
+    use Notifiable;
+    
     protected $table = 'reservations';
     protected $fillable = [
         'companie_id', 'client_id','check_percentage', 'reservation_reason_id', 'description', 'payment_status','active','note','observations','date_reservation','total','update_price','pendiente','payment_1','payment_2','payment_3','url_payment1','url_payment2','url_payment3'
