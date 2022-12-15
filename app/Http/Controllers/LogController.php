36a4672f88110eada99a13dacc857fe9c4679e18
appCulturaNorte <appculturanorte@gmail.com>
2022-12-15 00:23:18 -0500

    filtros logs

--- app/Http/Controllers/LogController.php
+++ app/Http/Controllers/LogController.php
@@ -6,6 +6,7 @@ use App\Models\Log;
 use App\Http\Controllers\Controller;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
+use App\User;
 
 class LogController extends Controller
 {
@@ -16,9 +17,23 @@ class LogController extends Controller
      */
     public function index()
     {
-        $logs_all = Log::where('created_at', '>=', Carbon::today())->paginate(15);
+        $logs_all = Log::where('created_at', '<=', Carbon::today());
+
+        if(isset($_GET['fromDate'],$_GET['toDate']) && $_GET['fromDate']!=""){
+            $logs_all = $logs_all->whereDate('created_at',">=",$_GET['fromDate'])->whereDate('created_at',"<=",$_GET['toDate']);
+        }
+
+        if(isset($_GET['client_id']) && $_GET['client_id']!=""){
+            $logs_all = $logs_all->where('user_id','=',$_GET['client_id']);
+        }
+
+        $logs_all = $logs_all->paginate(15);
+
+  
+
+        $users=User::all();
         //dd($logs_all);
-        return view('logs.index', compact('logs_all'));
+        return view('logs.index', compact('logs_all','users'));
     }
 
     /**
